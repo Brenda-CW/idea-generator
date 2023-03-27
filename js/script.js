de6075c660cb853e6b3bf707d533dc1f4c434f87
Brenda-CW <factotum4rays@gmail.com>
2023-03-27 11:37:40 -0500

    finished joke game

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const showIdea = document.querySelector(".show-idea"); //will create list and show ideas here
 let btns = document.querySelectorAll("#btn");
-const randomJokes = document.querySelector(".random-jokes"); //will put random jokes here
-const selectJokeNum = document.querySelector(".num-jokes"); //user chooses how many jokes to see
-
+const randomJokes = document.querySelector(".random-joke"); //will put random joke here
+const selectJokeNum = document.querySelector(".joke-btn"); //user clicks button to get joke
 // IDEA GENERATOR HERE
 // user clicks on activity button - html in <p> is cleared
 //query is run on activity type
@@ -47,15 +46,16 @@ const displayIdea = function(res){
 
 
 // JOKE GENERATOR HERE
-//user selects a number from selector
-//query is run & num of jokes returned
-//div is populated with num of jokes
+//user clicks button to show a joke
+//query is run & joke returned
+//div is populated with joke
 
 
-const getJokes = async function(numJokes){
-    const jokeRequest = await fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&lang=en&amount=${numJokes}`);
+const getJokes = async function(){
+    const jokeRequest = await fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&lang=en`);
     const data = await jokeRequest.json();
     console.log(data);
+    console.log(data.joke);
     displayJokes(data);
 }
 // getJokes(0);
@@ -63,40 +63,15 @@ const getJokes = async function(numJokes){
 const displayJokes = function(data){
     randomJokes.innerHTML = "";
 
-    for(let joke of data){
-        const thisJoke = data.joke;
-        console.log(thisJoke);
-
-        let jokeDiv = document.createElement("div");
-        jokeDiv.innerHTML = `
-        <div>
-            <p>${thisJoke}</p>
-
-        </div>
-        `;
-        randomJokes.append(jokeDiv);
-    }
-
-
-
-    // dataArr.forEach((joke) => {
-    //     let tellJoke = joke.jokes.joke;
-    //     console.log(tellJoke);
-    //     let jokeDiv = document.createElement("div");
-    //     jokeDiv.innerHTML = `
-    //     <div>
-    //         <p>${tellJoke}</p>
-
-    //     </div>
-    //     `;
-    // randomJokes.append(jokeDiv);
-
-    // });
-        
+    let jokeDiv = document.createElement("div");
+    jokeDiv.innerHTML = `
+     <p class='idea'>${data.joke}</p>
+    `;
+    randomJokes.append(jokeDiv);
 }
+        
 
-selectJokeNum.addEventListener('change', function(e){
-    const numJokes = e.target.value;
-    getJokes(numJokes);
+selectJokeNum.addEventListener('click', function(){
+    getJokes();
 });
 
